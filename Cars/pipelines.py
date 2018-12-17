0ffcfd5d7458d44584f031fc72351f4d3bd4d526
root <marwen.benhriz@gmail.com>
2018-12-17 17:42:07 +0100

    update piplines to save in mysql db

--- Cars/pipelines.py
+++ Cars/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sys
+import MySQLdb
+import hashlib
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
 
 
 class CarsPipeline(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect('host', 'user', 'passwd', 'dbname', charset="utf8",use_unicode=True)
+	self.cursor = self.conn.cursor()
+
+
     def process_item(self, item, spider):
         return item
